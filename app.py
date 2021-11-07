8252540f2cf6ccf6acce7d258ae33ace100da92c
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-11-06 23:52:56 -0400

    Built video+image upload system

--- app.py
+++ app.py
@@ -1,24 +1,55 @@
+from numpy import cbrt
 import streamlit as st
 import time
 from PIL import Image
-
-
-# with st.empty():
-#     for seconds in range(10):
-#         st.write(f'{seconds} seconds haves passed')
-#         time.sleep(1)
-#     st.write('COMPLETE!')
+import cv2
+import tempfile
 
 def detect_image(image_path):
     raw_image = Image.open(image_path).convert('RGB')
     image_pil = raw_image.resize((720,400))
     return image_pil
 
-video_file = open('test-files/video2.mp4', 'rb')
-video_bytes = video_file.read()
-st.video(video_bytes)
+def detect_video(vidcap, counter):
+    success,image = vidcap.read()
+    count = 0
+    
+    
+    imgs = []
+    frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT)) 
 
-with st.empty():
-    for seconds in range(1, 6):
-        st.image(detect_image(f'test-files/out{seconds}.jpg'))
-        time.sleep(0.8)
+    for i in range(1, frame_count + 1):
+        success,image = vidcap.read()
+        imgs.append(image)
+        count += 1
+
+    preds = []
+    for frame in range(1, frame_count-1, counter):
+        pil_converted = Image.fromarray(cv2.cvtColor(imgs[frame], cv2.COLOR_BGR2RGB))
+        image_pil = pil_converted.resize((720,400))
+
+        # plt.figure(figsize=(15,7))
+        # output = scan_image(image_pil, obj_thresh=0.55)
+        # plt.title(f'Frame {frame}, {output}')
+        # plt.imshow(output)
+        # preds.append(output)
+        preds.append(image_pil)
+
+    return preds
+
+file = st.file_uploader("Upload A Video")
+
+if file:
+    tfile = tempfile.NamedTemporaryFile(delete=False)
+    tfile.write(file.read())
+    vidcap = cv2.VideoCapture(tfile.name)
+    st.write(vidcap)
+    st.write(f'`Total Frames: {int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))}`')
+    frames = detect_video(vidcap, 1)
+
+    if st.button('Run'):
+        st.markdown('----')
+        with st.empty():
+            for i in range(len(frames)):
+                st.image(frames[i])
+                time.sleep(0.01)
