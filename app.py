c83d9c6c646334337a163ef65c799a20ec72e172
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-11-06 22:47:31 -0400

    displayed video

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def detect_image(image_path):
     image_pil = raw_image.resize((720,400))
     return image_pil
 
+video_file = open('test-files/video2.mp4', 'rb')
+video_bytes = video_file.read()
+st.video(video_bytes)
+
 with st.empty():
     for seconds in range(1, 15):
         st.image(detect_image(f'test-files/image{seconds}.jpg'))
